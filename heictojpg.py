4fd097d31ca6f0f9783e6f9616db437341a1adf5
Marcos Santiago <hello@marcossantiago.com>
2024-05-01 08:44:56 -0400

    Update heictojpg.py

--- heictojpg.py
+++ heictojpg.py
@@ -1,37 +1,60 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 import subprocess
 import os
 
 def open_heic():
-    file_path = filedialog.askopenfilename(filetypes=[("HEIC files", "*.heic")])
-    if file_path:
-        input_path.set(file_path)
+    file_paths = filedialog.askopenfilenames(
+        title="Seleccionar archivos",
+        filetypes=[("Image files", "*.heic *.HEIC *.jfif *.JFIF")]
+    )
+    if file_paths:
+        # Unimos las rutas y convertimos las extensiones a minúsculas
+        normalized_paths = [path.lower() for path in file_paths]
+        input_path.set(";".join(normalized_paths))
 
 def save_jpg():
-    file_path = filedialog.asksaveasfilename(defaultextension=".jpg", filetypes=[("JPEG files", "*.jpg")])
-    if file_path:
-        output_path.set(file_path)
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        output_path.set(folder_path)
 
 def convert_heic_to_jpg():
-    heic_path = os.path.abspath(input_path.get())
-    jpg_path = os.path.abspath(output_path.get())
-    if not heic_path or not jpg_path:
-        messagebox.showerror("Error", "Por favor selecciona un archivo HEIC y especifica la ubicación para guardar el JPG.")
+    file_paths = input_path.get().split(";")
+    folder_jpg = output_path.get()
+    if not file_paths or not folder_jpg:
+        messagebox.showerror("Error", "Por favor selecciona los archivos y especifica la carpeta para guardar los JPGs.")
         return
+
+    progress_bar['maximum'] = len(file_paths)
+    progress_bar['value'] = 0
+    root.update_idletasks()
+
     magick_path = r'C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe'
-    try:
-        subprocess.run([magick_path, 'convert', heic_path, jpg_path], check=True)
-        messagebox.showinfo("Éxito", "El archivo se ha convertido exitosamente!")
-    except subprocess.CalledProcessError as e:
-        messagebox.showerror("Error", f"Falló la conversión del archivo: {e}")
-    except FileNotFoundError as e:
-        messagebox.showerror("Error", f"No se encontró el ejecutable de ImageMagick: {e}")
+    errors = []
+
+    for file_path in file_paths:
+        file_extension = os.path.splitext(file_path)[1]
+        if file_extension in [".heic", ".HEIC"]:
+            jpg_path = os.path.join(folder_jpg, os.path.basename(file_path).replace(".heic", ".jpg").replace(".HEIC", ".jpg"))
+        elif file_extension in [".jfif", ".JFIF"]:
+            jpg_path = os.path.join(folder_jpg, os.path.basename(file_path).replace(".jfif", ".jpg").replace(".JFIF", ".jpg"))
+        
+        try:
+            subprocess.run([magick_path, file_path, jpg_path], shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            errors.append(f"Falló la conversión del archivo {file_path}: {e}")
+        progress_bar['value'] += 1
+        root.update_idletasks()
+
+    if errors:
+        messagebox.showerror("Error en la conversión", "\n".join(errors))
+    else:
+        messagebox.showinfo("Éxito", "Todos los archivos se han convertido exitosamente!")
 
 # Configuración de la GUI
 root = tk.Tk()
-root.title("Convertidor de HEIC a JPG")
-root.config(bg="#333333")  # Color de fondo de la ventana
+root.title("HEIC y JFIF a JPG | @m4rcos | v1.1")
+root.config(bg="#333333")
 
 # Variables para las rutas de archivo
 input_path = tk.StringVar()
@@ -39,21 +62,32 @@ output_path = tk.StringVar()
 
 # Estilos
 style = {"font": ("Arial", 12), "bg": "#333333", "fg": "#ffffff"}
-button_style = {"font": ("Arial", 12), "bg": "#0078D7", "fg": "#ffffff", "padx": 10, "pady": 5}
+button_style = {
+    "font": ("Arial", 12), 
+    "bg": "#0078D7", 
+    "fg": "#ffffff", 
+    "padx": 10, 
+    "pady": 5
+}
+
 
 # Widgets
-tk.Label(root, text="Selecciona el archivo HEIC:", **style).pack(pady=(10, 0))
+tk.Label(root, text="Selecciona los archivos HEIC o JFIF:", **style).pack(pady=(10, 0))
 entry_heic = tk.Entry(root, textvariable=input_path, width=50, **style)
 entry_heic.pack(pady=(5, 10))
 
 tk.Button(root, text="Buscar", command=open_heic, **button_style).pack()
 
-tk.Label(root, text="Guardar el JPG como:", **style).pack(pady=(10, 0))
+tk.Label(root, text="Guardar los JPGs en la carpeta:", **style).pack(pady=(10, 0))
 entry_jpg = tk.Entry(root, textvariable=output_path, width=50, **style)
 entry_jpg.pack(pady=(5, 10))
 
-tk.Button(root, text="Guardar como", command=save_jpg, **button_style).pack()
+tk.Button(root, text="Seleccionar carpeta", command=save_jpg, **button_style).pack()
 
-tk.Button(root, text="Convertir", command=convert_heic_to_jpg, **button_style).pack(pady=(10, 20))
+tk.Button(root, text="Convertir", command=convert_heic_to_jpg, **button_style).pack(pady=(10, 5))
+
+# Barra de progreso
+progress_bar = ttk.Progressbar(root, orient="horizontal", length=400, mode="determinate")
+progress_bar.pack(pady=(5, 20))
 
 root.mainloop()
